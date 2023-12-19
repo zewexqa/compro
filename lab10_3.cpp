3459e222fe6a52850b6fb6f0eb71fe30dd22aa8d
zewexqa <154309211+zewexqa@users.noreply.github.com>
2023-12-19 17:11:30 +0700

    Update lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,25 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    int count = 0;
+    float sum = 0;
+    float sum_of_square =  0;
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source,textline)){
+        sum += atof(textline.c_str());
+        sum_of_square += pow(atof(textline.c_str()),2);
+        count++;
+    }
+    cout << "Number of data = "<< count<<'\n';
+    cout << setprecision(3);
+    cout << "Mean = "<< sum/count << '\n';
+    cout << "Standard deviation = "<< sqrt((sum_of_square/count)-pow(sum/count,2));
+}
